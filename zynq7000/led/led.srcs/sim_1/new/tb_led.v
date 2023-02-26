d301d784d08dad2fbf51f565e54886e55ee32b47
Yuan Jin <jin_yuan0928@163.com>
2023-02-26 11:24:53 +0100

    first commit

---
+++ zynq7000/led/led.srcs/sim_1/new/tb_led.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2023/02/26 11:10:55
+// Design Name: 
+// Module Name: tb_led
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_led(
+
+    );
+endmodule
